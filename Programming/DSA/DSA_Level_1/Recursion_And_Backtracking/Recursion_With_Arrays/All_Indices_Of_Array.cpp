73e1bad58c337471d2acafddeb5f1eaa865b065c
AayushInterwork <aayush@interwork.biz>
2022-01-19 12:57:34 +0530

    Completed Full Recursion in Arrays (Done All Indices of Array , Factorial , Fibonacci) (Day 8)

--- Programming/DSA/DSA_Level_1/Recursion_And_Backtracking/Recursion_With_Arrays/All_Indices_Of_Array.cpp
+++ Programming/DSA/DSA_Level_1/Recursion_And_Backtracking/Recursion_With_Arrays/All_Indices_Of_Array.cpp
@@ -1,40 +1,37 @@
 #include <iostream>
 using namespace std;
 
-int *allIndices(int arr[], int idx, int x, int n, int fsf)
+void allIndices(int arr[], int idx, int d, int n)
 {
-    // write your code here
     if (idx == n)
     {
-        int arr2[fsf];
-        return arr2;
+        return;
     }
-    int iarr[fsf];
-    if (arr[idx] == x)
+    if (arr[idx] == d)
     {
-        iarr = allIndices(arr, idx + 1, x, n, fsf + 1);
-        iarr[fsf] = idx;
+        cout << idx << " , ";
+        allIndices(arr, idx + 1, d, n);
     }
     else
     {
-        iarr = allIndices(arr, idx + 1, x, n, fsf);
+        allIndices(arr, idx + 1, d, n);
     }
 }
 
 int main()
 {
     int n;
+    cout << "Enter Size of Array : ";
     cin >> n;
     int d;
     int arr[n];
+    cout << "Enter Elements in Array : ";
     for (int i = 0; i < n; i++)
         cin >> arr[i];
+    cout << "Enter Element to Find Occurences in Array : ";
     cin >> d;
-    int *arr2 = allIndices(arr, 0, d, n, 0);
-    // int fsf = allIndices;
-    for (int i = 0; i < 10; i++)
-    {
-        cout << arr2[i] << endl;
-    }
-    // cout << p << endl;
+
+    cout << d << " is present at Index ";
+    allIndices(arr, 0, d, n);
+    cout << "\n";
 }
